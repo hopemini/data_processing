3598dcb69bd8c6fa6cb1224bc1420dac9054dcb8
Sungmin Choi <sungmin.choi@lge.com>
2022-04-12 14:53:34 +0900

    Update find_mean

--- jideca_params/find_mean.py
+++ jideca_params/find_mean.py
@@ -1,6 +1,8 @@
 import glob
 import argparse
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 par = argparse.ArgumentParser()
 par.add_argument("-d", "--dir", default="beta", choices=["beta", "gamma"],
@@ -11,6 +13,7 @@ evaluation_methods = ['ari', 'nmi', 'purity']
 ari_best_result = dict()
 nmi_best_result = dict()
 purity_best_result = dict()
+
 
 total = 0
 for dirs in glob.glob(args.dir + '/*'):
@@ -38,12 +41,13 @@ for dirs in glob.glob(args.dir + '/*'):
                         purity_best_result[k] = float(v.split('%')[0])
     total += 1
 
+
 for k, v in ari_best_result.items():
     ari_best_result[k] = round(v / total, 3)
 for k, v in nmi_best_result.items():
     nmi_best_result[k] = round(v / total, 3)
 for k, v in purity_best_result.items():
-    purity_best_result[k] = round(v / total, 1)
+    purity_best_result[k] = round(v / (total * 100), 3)
 
 print(ari_best_result)
 print(nmi_best_result)
@@ -55,18 +59,93 @@ df_purity = pd.DataFrame(list(purity_best_result.items()), columns=['parameters'
 
 
 df_ari['simple'] = df_ari['parameters'].apply(lambda x: x.split('_')[1].split('r')[0][1:])
-df_ari['simple'] = df_ari['simple'].apply(lambda x: float('0.' + x[1]) if x[0] == '0' else float(x))
+#df_ari['simple'] = df_ari['simple'].apply(lambda x: float('0.' + x[1]) if x[0] == '0' else float(x))
+df_ari['simple'] = df_ari['simple'].apply(lambda x: '0.' + x[1] if x[0] == '0' else x)
 df_ari['types'] = df_ari['parameters'].apply(lambda x:x.split('_')[1][-2:])
+df_ari['types'] = df_ari['types'].apply(lambda x: 'real' if x == 're' else 'semantic')
 
 df_nmi['simple'] = df_nmi['parameters'].apply(lambda x:x.split('_')[1].split('r')[0][1:])
 #df_nmi['simple'] = df_nmi['simple'].apply(lambda x: float('0.' + x[1]) if x[0] == '0' else float(x))
 df_nmi['simple'] = df_nmi['simple'].apply(lambda x: '0.' + x[1] if x[0] == '0' else x)
 df_nmi['types'] = df_nmi['parameters'].apply(lambda x:x.split('_')[1][-2:])
+df_nmi['types'] = df_nmi['types'].apply(lambda x: 'real' if x == 're' else 'semantic')
 
 df_purity['simple'] = df_purity['parameters'].apply(lambda x:x.split('_')[1].split('r')[0][1:])
-df_purity['simple'] = df_purity['simple'].apply(lambda x: float('0.' + x[1]) if x[0] == '0' else float(x))
+#df_purity['simple'] = df_purity['simple'].apply(lambda x: float('0.' + x[1]) if x[0] == '0' else float(x))
+df_purity['simple'] = df_purity['simple'].apply(lambda x: '0.' + x[1] if x[0] == '0' else x)
 df_purity['types'] = df_purity['parameters'].apply(lambda x:x.split('_')[1][-2:])
+df_purity['types'] = df_purity['types'].apply(lambda x: 'real' if x == 're' else 'semantic')
 
+
+#df_ari.to_csv('ari_means.csv')
+#df_nmi.to_csv('nmi_means.csv')
+#df_purity.to_csv('purity_means.csv')
+df_purity.loc[df_purity['types'] == 'real'].sort_values(by='simple')
+
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 14
+
+plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+# NMI
+plt.figure(figsize=(8,5))
+#sns.lineplot(x='simple', y='nmi', data=df_nmi.loc[df_nmi['types'] == 're'].sort_values(by='simple'))
+#sns.lineplot(x='simple', y='nmi', data=df_nmi.loc[df_nmi['types'] == 'se'].sort_values(by='simple'))
+sns.lineplot(x='simple', y='nmi', data=df_nmi.sort_values(by='simple'), hue='types', style='types', markers=True, markersize=14)
+for i in df_nmi.groupby('types'):
+    for x,y,m in i[1][['simple','nmi','nmi']].values:
+        if x == '0.1': i = 0
+        elif x == '0.2': i = 1
+        elif x == '0.5': i = 2
+        elif x == '1': i = 3
+        else: i = 4
+        plt.text(i - 0.2 , y + 0.0051, m)
+plt.ylim(0.42, 0.56)
+plt.ylabel('NMI', fontsize=16)
+plt.xlabel(None)
+plt.savefig(args.dir + '_nmi_means.png', transparent=True)
+
+# ARI
+plt.figure(figsize=(8,5))
+sns.lineplot(x='simple', y='ari', data=df_ari.sort_values(by='simple'), hue='types', style='types', markers=True, markersize=14)
+for i in df_ari.groupby('types'):
+    for x,y,m in i[1][['simple','ari','ari']].values:
+        if x == '0.1': i = 0
+        elif x == '0.2': i = 1
+        elif x == '0.5': i = 2
+        elif x == '1': i = 3
+        else: i = 4
+        plt.text(i - 0.2 , y + 0.0051, m)
+plt.ylim(0.16, 0.32)
+plt.ylabel('ARI', fontsize=16)
+plt.xlabel(None)
+plt.savefig(args.dir + '_ari_means.png', transparent=True)
+
+# Purity
+plt.figure(figsize=(8,5))
+#sns.lineplot(x='simple', y='purity', data=df_purity.loc[df_purity['types'] == 're'].sort_values(by='simple'))
+#sns.lineplot(x='simple', y='purity', data=df_purity.loc[df_purity['types'] == 'se'].sort_values(by='simple'))
+#sns.lineplot(x='simple', y='purity', data=df_purity.sort_values(by='simple'), hue='types', style='types', markers=['o', '^'], markersize=14)
+sns.lineplot(x='simple', y='purity', data=df_purity.sort_values(by='simple'), hue='types', style='types', markers=True, markersize=14)
+for i in df_purity.groupby('types'):
+    for x,y,m in i[1][['simple','purity','purity']].values:
+        if x == '0.1': i = 0
+        elif x == '0.2': i = 1
+        elif x == '0.5': i = 2
+        elif x == '1': i = 3
+        else: i = 4
+        plt.text(i - 0.2 , y + 0.0051, m)
+plt.ylim(0.20, 0.36)
+plt.ylabel('Purity', fontsize=16)
+plt.xlabel(None)
+plt.savefig(args.dir + '_purity_means.png', transparent=True)
 
 df_ari.to_csv(args.dir + '_ari_means.csv')
 df_nmi.to_csv(args.dir + '_nmi_means.csv')
