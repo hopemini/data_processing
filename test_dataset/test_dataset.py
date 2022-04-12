5c588c1a07c92c6535cf1831b134ab6accd667ba
Sungmin Choi <hopemini@gmail.com>
2022-04-13 00:07:05 +0900

    Update test_dataset

--- test_dataset/test_dataset.py
+++ test_dataset/test_dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 cat_c = []
 for i in range(23):
@@ -9,16 +10,114 @@ cat_c_num = [30, 40, 15, 10, 40, 35, 30, 30, 22, 5, 45, 30, 35, 45, 30, 30, 30, 
 
 cat_r = []
 for i in range(34):
-    cat_r.append('c'+str(i+1))
+    cat_r.append('r'+str(i+1))
 cat_r_num = [30, 40, 15, 10, 40, 35, 30, 30, 22, 5, 45, 30, 35, 45, 30, 30, 30, 30, 17, 40, 8, 42, 18, 40, 30, 15, 25, 45, 52, 30, 6, 30, 35, 35]
 
-cat_c_num.describe()
+df_c = pd.DataFrame({'C23':cat_c, 'Number':cat_c_num})
+df_r = pd.DataFrame({'R34':cat_r, 'Number':cat_r_num})
 
-#plt.figure(figsize=(10, 8))
+#df_c['Number'].describe()
+
+SMALL_SIZE = 8
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+plt.figure(figsize=(15,4))
 plt.xticks(rotation=90)
-plt.bar(cat_c, cat_c_num,)
-#sns.barplot(cat_c, cat_c_num,)
-plt.show()
+sns.barplot(x='C23', y='Number', data=df_c, color='#3F466E')
+#ax.bar_label(ax.containers[0])
+for i, v in enumerate(df_c['Number']):
+    plt.text(i - 0.22 , v + 0.8, v)
+plt.xlabel(None)
+plt.ylabel(None)
+plt.ylim(0, 50)
+plt.savefig('c23_bar.png', transparent=True)
 
-sns.violinplot(cat_c_num)
-plt.show()
+plt.figure(figsize=(15,4))
+plt.xticks(rotation=90)
+sns.barplot(x='R34', y='Number', data=df_r, color='#3F466E')
+#ax.bar_label(ax.containers[0])
+for i, v in enumerate(df_r['Number']):
+    plt.text(i - 0.35 , v + 0.8, v)
+plt.xlabel(None)
+plt.ylabel(None)
+plt.ylim(0, 60)
+plt.savefig('r34_bar.png', transparent=True)
+
+plt.figure(figsize=(15,4))
+gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1]) 
+plt.subplot(gs[0])
+plt.xticks(rotation=90)
+ax = sns.barplot(x='C23', y='Number', data=df_c, color='#3F466E')
+#ax.bar_label(ax.containers[0])
+for i, v in enumerate(df_c['Number']):
+    plt.text(i - 0.32 , v + 0.5, v)
+ax.set(xlabel=None)
+ax.set(ylabel=None)
+ax.set(ylim=(0, 50))
+plt.subplot(gs[1])
+ax = sns.distplot(df_c['Number'])
+ax.set(xlabel=None)
+ax.set(ylabel=None)
+plt.savefig('c23_dist.png', transparent=True)
+
+
+plt.figure(figsize=(15,3))
+plt.subplot(1,2,1)
+ax = sns.distplot(df_c['Number'])
+ax.set(xlabel=None)
+plt.subplot(1,2,2)
+ax = sns.boxplot(df_c['Number'])
+ax.set(xlabel=None)
+
+
+plt.figure(figsize=(15,4))
+gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1]) 
+plt.subplot(gs[0])
+plt.xticks(rotation=90)
+ax = sns.barplot(x='R34', y='Number', data=df_r, color='#3F466E')
+#ax.bar_label(ax.containers[0])
+for i, v in enumerate(df_r['Number']):
+    plt.text(i - 0.44 , v + 0.5, v)
+ax.set(xlabel=None)
+ax.set(ylabel=None)
+ax.set(ylim=(0, 60))
+plt.subplot(gs[1])
+ax = sns.distplot(df_r['Number'])
+ax.set(xlabel=None)
+ax.set(ylabel=None)
+plt.savefig('r34_dist.png', transparent=True)
+
+
+plt.figure(figsize=(15,3))
+plt.subplot(1,2,1)
+ax = sns.distplot(df_r['Number'])
+ax.set(xlabel=None)
+plt.subplot(1,2,2)
+ax = sns.boxplot(df_r['Number'])
+ax.set(xlabel=None)
+
+plt.figure(figsize=(15,3))
+plt.subplot(1,2,1)
+sns.boxplot(df_c['Number'])
+plt.subplot(1,2,2)
+sns.boxplot(df_r['Number'])
+
+#sns.displot(df_c['Number'], kde=True)
+#sns.displot(df_r['Number'], kde=True)
+
+plt.figure(figsize=(10,5))
+sns.distplot(df_c['Number'])
+sns.distplot(df_r['Number'])
+plt.legend(labels=["C23","R34"], title = "Category")
+plt.xlabel(None)
+plt.ylabel(None)
+plt.savefig('c23_r34_dist.png', transparent=True)
